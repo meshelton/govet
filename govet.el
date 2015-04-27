d15dc04addeefa5a70f46b4bfcc83d7d463222b9
Michael Eugene Shelton <michael.e.shelton@gmail.com>
2015-04-27 19:36:29 -0400

    first run at go vet

---
+++ govet.el
@@ -0,0 +1,49 @@
+;;; govet.el --- linter/problem finder for the Go source code
+
+;; URL: https://godoc.org/golang.org/x/tools/cmd/vet
+
+;;; Commentary:
+
+;; To install golint, add the following lines to your .emacs file:
+;;   (add-to-list 'load-path "PATH CONTAINING govet.el" t)
+;;   (require 'golint)
+;;
+;; After this, type M-x golint on Go source code.
+;;
+;; Usage:
+;;   C-x `
+;;     Jump directly to the line in your code which caused the first message.
+;; 
+;;   For more usage, see Compilation-Mode:
+;;     http://www.gnu.org/software/emacs/manual/html_node/emacs/Compilation-Mode.html
+
+;;; Code:
+(require 'compile)
+
+(defun go-vet-buffer-name (mode) 
+ "*Govet*") 
+
+(defun govet-process-setup ()
+  "Setup compilation variables and buffer for `govet'."
+  (run-hooks 'govet-setup-hook))
+
+(define-compilation-mode govet-mode "govet"
+  "Govet is a veter for Go source code."
+  (set (make-local-variable 'compilation-scroll-output) nil)
+  (set (make-local-variable 'compilation-disable-input) t)
+  (set (make-local-variable 'compilation-process-setup-function)
+       'govet-process-setup)
+)
+
+;;;###autoload
+(defun govet ()
+  "Run govet on the current file and populate the fix list. Pressing C-x ` will jump directly to the line in your code which caused the first message."
+  (interactive)
+  (compilation-start
+   (mapconcat #'shell-quote-argument
+              (list "go vet" (expand-file-name buffer-file-name)) " ")
+   'govet-mode))
+
+(provide 'govet)
+
+;;; govet.el ends here
